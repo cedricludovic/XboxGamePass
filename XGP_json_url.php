630e5b3ff98a853a68d99e284ca8d3af372eac70
cedricludovic <cedricludovic@hotmail.com>
2022-09-01 08:29:54 +0200

    MaJ des chemins

--- XGP_json_url.php
+++ XGP_json_url.php
@@ -1,12 +1,12 @@
 <?php
 
-$outfile= 'C:\wamp64\www\jsonphp\json\xgp_games.json';
+$outfile= '.\json\xgp_games.json';
 $url='https://catalog.gamepass.com/sigls/v2?id=f6f1f99f-9b49-4ccd-b3bf-4d9767a77f5e&language=fr-fr&market=FR';
 $json = json_encode(array_slice(json_decode(file_get_contents($url), true), 1));
 
 if($json) { 
     if(file_put_contents($outfile, $json, FILE_APPEND)) {
-      echo "Saved JSON fetched from “{$url}” as “C:\Perso\xbox\jsonphp\jsonphp\json\$outfile”.";
+      echo "Saved JSON fetched from “{$url}” as “$outfile”.";
     }
     else {
       echo "Unable to save JSON to “{$outfile}”.";
